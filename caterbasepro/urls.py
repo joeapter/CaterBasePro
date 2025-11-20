4e53da77b3b67a0272ed3db3d90fb89a98dfb0e0
Joe Apter <joeapter@gmail.com>
2025-11-20 05:06:45 +0200

    Redirect site root to admin

--- caterbasepro/urls.py
+++ caterbasepro/urls.py
@@ -18,7 +18,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='admin:index', permanent=False)),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
